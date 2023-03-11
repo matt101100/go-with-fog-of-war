46a63f12f8f954d316ba8c29d4ab768218871968
matt <mlaz7837@uni.sydney.edu.au>
2023-03-11 10:18:39 +0000

    s

--- gitm.c
+++ gitm.c
@@ -60,7 +60,7 @@ int get_line(char *buffer, size_t buffer_len) {
  * returns 1 if string is valid, 0 otherwise
 */ 
 int is_place_command(char input[]) {
-    if (strlen(input) < MIN_PLACE_CMD_LEN - 2) {
+    if (strlen(input) < MIN_PLACE_CMD_LEN - 1) {
         return 0;
     }
 
